19876a9f1c2e68c97c6418a23744f68dd36b68e8
Lara Aguilar <72893552+laraguilar@users.noreply.github.com>
2022-11-08 13:11:24 +0000

    Leitura do arquivo binário

--- main.py
+++ main.py
@@ -6,4 +6,25 @@ Professor: Hilário Seibel
 Grupo: Gabriely Moro e Lara Aguilar
 '''
 
-if
+import pickle
+import os
+
+
+
+
+
+def readArq():
+  arq = 'entrada10.bin'
+  if os.path.isfile(arq):
+    with open('entrada10.bin', 'rb') as arq:
+      dic = pickle.load(arq)
+    return dic
+    
+
+
+def main():
+  print(readArq())
+  return 0
+
+if __name__ == '__main__':
+  main()
